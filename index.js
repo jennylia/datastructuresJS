73db27d63fb485d4ef6e48df22915c61f727cbc6
Jenny lian <jennylian100@gmail.com>
2016-02-21 16:34:07 -0800

    OMG this works buts defeats the purpose of making one from scratch

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 var binaryTree = require('./BinaryTree');
 
+var FibonacciHeap = require("js-data-structures").FibonacciHeap;
+var heap = new FibonacciHeap();
+heap.insert(3);
+heap.insert(7);
+heap.insert(8);
+heap.insert(1);
+heap.insert(2);
+
+while (!heap.isEmpty()) {
+    console.log(heap.extractMinimum());
+}
