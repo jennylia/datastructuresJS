1005ea900e303150eb17a49c161f2a337ac382cb
Jenny lian <jennylian100@gmail.com>
2016-02-21 18:32:17 -0800

    this is some weirdness I haven't encountered, but basically you can declare functions here and execute them

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-var binaryTree = require('./BinaryTree');
+/**
+ * @module index
+ * @license MIT Copyright 2015 Daniel Imms (http://www.growingwiththeweb.com)
+ */
+'use strict';
+//
+//if (typeof exports === 'object' && typeof define !== 'function') {
+//    var define = function (factory) {
+//        factory(require, exports, module);
+//    };
+//}
+//
+//var define = function (factory) {
+//    factory(require, exports, module);
+//};
+//
+//define(function (require, exports, module) {
+//    module.exports = {
+//        BinarySearchTree: require('./lib/bst'),
+//    };
+//});
 
+var BinarySearchTree = require('./lib/bst');
+
+var newTree = new BinarySearchTree();
+newTree.add(5);
+newTree.add(6);
+//var s = newTree.size();
+
+//console.log(s);
