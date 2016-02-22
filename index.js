6ac05021ea643ef0397b9d13d3c9aeb79ffd2d71
Jenny lian <jennylian100@gmail.com>
2016-02-21 18:44:25 -0800

    copied the data structures over

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var binaryTree = require('./BinaryTree');
 
-var FibonacciHeap = require("js-data-structures").FibonacciHeap;
-var heap = new FibonacciHeap();
-heap.insert(3);
-heap.insert(7);
-heap.insert(8);
-heap.insert(1);
-heap.insert(2);
+//var FibonacciHeap = require("js-data-structures").FibonacciHeap;
+//var heap = new FibonacciHeap();
+//heap.insert(3);
+//heap.insert(7);
+//heap.insert(8);
+//heap.insert(1);
+//heap.insert(2);
+//
+//while (!heap.isEmpty()) {
+//    console.log(heap.extractMinimum());
+//}
 
-while (!heap.isEmpty()) {
-    console.log(heap.extractMinimum());
-}
+//Today is a tree day
+var BinaryTree =
